77d6431dd3571ec1528aa3690fa46c62badf375a
Guoxh <AGuoxh@163.com>
2019-12-18 16:59:44 +0800

    首页 分类 购物车  样式

---
+++ pages/orderDetail/orderDetail.js
@@ -0,0 +1,78 @@
+// pages/orderDetail/orderDetail.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    statusIcon: "/assets/icon/notpay.png",
+    statusTitle: "等待买家付款",
+    statusMessage: "还剩23小时40分自动关闭",
+    telName: "张三",
+    telNum: 13754945600,
+    telAddress: "广西北海海城区北京路323号商铺北海k昆明路案扥55号",
+    storeName: "老张商铺",
+    imgUrl: "/assets/img/goods-list2.png",
+    goodsText: "三只松鼠爆浆曲奇160g烘焙甜品饼干手工网红爆......",
+    goodsPrice: "¥12.00",
+    goodsNum: 3,
+    logisticsMessage: "广州转运中心公司 已出发, 下一站 南宁转运公司",
+    logisticsDate: "2019-10-22 11:12:55"
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
---
+++ pages/sureOrder/sureOrder.js
@@ -0,0 +1,73 @@
+// pages/sureOrder/sureOrder.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    telName: "张载",
+    telNum: "17983218500",
+    telAddress: "广西北海海城区北京路323号商铺北海卡萨当减肥狂23号",
+    storeName: "老张商铺",
+    imgUrl: "/assets/img/goods-list2.png",
+    goodsText: "三只松鼠爆浆曲奇160g烘焙甜品饼干手工网红爆......",
+    goodsPrice: "¥12.00",
+    goodsNum: 3
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
